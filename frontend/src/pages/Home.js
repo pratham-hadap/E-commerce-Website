df6a9f36d5d29fcea99792b7dba60a8698b6d4ef
pratha-hadap <prathamhadap@gmail.com>
2024-09-24 23:16:56 +0530

    changes in home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -142,10 +142,11 @@ export const Home = ({ searchParams }) => {
   const { cart } = useSelector((state) => state);
 
   useEffect(() => {
+    const Backend_url = process.env.Backend_url
     const fetchProducts = async () => {
       try {
         // const response = await axios.get('http://localhost:9000/api/v1/getProduct');
-        const response = await axios.get(process.env.Backend_url);
+        const response = await axios.get(`${Backend_url}/getProducts`);
         // const response = await axios.get('https://e-commerce-website-backend-xfir.onrender.com/api/v1');
         // const response = await axios.get('https://e-commerce-website-backend-xfir.onrender.com');
         setProducts(response.data.data);

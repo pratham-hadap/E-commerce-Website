1b54b2ac859c4648f1a4e81d64e1d911ad1d03fb
pratha-hadap <prathamhadap@gmail.com>
2024-09-25 16:47:06 +0530

    chages in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,57 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import {BrowserRouter} from 'react-router-dom'
+// import './index.css';
+// import App from './App';
+// import { Provider } from "react-redux";
+// import { store } from './redux/Store';
+// import { Toaster } from 'react-hot-toast';
+// import AppContextProvider from './context/AppContext';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+//     <AppContextProvider>
+//         <BrowserRouter>
+//            <Provider store={store} >
+//                 <App/>
+//                 <Toaster></Toaster>
+//            </Provider>
+//         </BrowserRouter>
+//     </AppContextProvider>
+    
+// );
+
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import { store } from './redux/Store';
 import { Toaster } from 'react-hot-toast';
 import AppContextProvider from './context/AppContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// Create a QueryClient instance
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
     <AppContextProvider>
         <BrowserRouter>
-           <Provider store={store} >
-                <App/>
-                <Toaster></Toaster>
-           </Provider>
+            <Provider store={store}>
+                {/* Wrap your app with QueryClientProvider */}
+                <QueryClientProvider client={queryClient}>
+                    <App />
+                    <Toaster />
+                </QueryClientProvider>
+            </Provider>
         </BrowserRouter>
     </AppContextProvider>
-    
 );
-
